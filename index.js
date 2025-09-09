455a412e750f92839fdd7ad7971f114a4658616b
tonsojun <josephjun@gmail.com>
2025-09-09 10:30:34 -0700

    Stuck

--- index.js
+++ index.js
@@ -7,25 +7,29 @@
 // Once all information is received then show an alert like: 
 // "5 people are traveling with 6 suitcases in total."
 
-const numberOfPeople = prompt("How many people are going on a trip?")
-console.log(numberOfPeople);
+const numberOfAdults = prompt("How many people are going on a trip?");
+const numberOfKids = prompt("How many kids are in the party?");
+const totalNumberOfPeople = numberOfAdults + numberOfKids;
+console.log("totalNumberOfPeople is " + totalNumberOfPeople);
 //create a function that will prompt each number of people how many suitcases. 
 
 const bagCheck = (numberOfPeople) => {
 	
 	let totalBagsPerPerson = 0;
-	let ticketPrice = 0;
+	let adultTicketPrice = 0;
+	let kidTicketPrice = 0;
 	let suitcaseCost = 0;
-	for(let i = 1; i <= numberOfPeople; i++) {
+	for(let i = 1; i <= totalNumberOfPeople; i++) {
 		let howManyBags = parseInt(prompt(`How many suitcases is person ${i} bringing?`));
 		console.log(howManyBags);
 		console.log("in the function");
 	  totalBagsPerPerson += howManyBags;
-		ticketPrice = howManyBags* 80;
+		adultTicketPrice = howManyAdultBags * 80;
+		kidTicketPrice = howManyKidBags * 50 
 		suitcaseCost = totalBagsPerPerson * 40
 		console.log(totalBagsPerPerson);
 	}
-	alert(`${numberOfPeople} people are traveling with ${totalBagsPerPerson} suitcases in total. Total Price: $${ticketPrice + suitcaseCost}`);
+	alert(`${numberOfPeople} people are traveling with ${totalBagsPerPerson} suitcases in total. Total Price: $${adultTicketPrice + kidTicketPrice + suitcaseCost}`);
 
 }
 bagCheck(numberOfPeople);
